15134463605c119cce2cbaff2198e6ef7209455d
shai <yayaya142@gmail.com>
2024-02-07 20:20:42 +0200

    shai commit

---
+++ cmake-build-debug/CMakeFiles/Desktop.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Desktop.dir/flight.c.obj"
+  "CMakeFiles/Desktop.dir/flight.c.obj.d"
+  "CMakeFiles/Desktop.dir/main.c.obj"
+  "CMakeFiles/Desktop.dir/main.c.obj.d"
+  "Desktop.exe"
+  "Desktop.exe.manifest"
+  "Desktop.pdb"
+  "libDesktop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
