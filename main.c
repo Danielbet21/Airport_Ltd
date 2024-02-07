1a170501a2e0f45a9d48fe2836c1260b8dff4dc7
shai <yayaya142@gmail.com>
2024-02-07 21:24:32 +0200

    add new test form master

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main() {
     printf("hell world from shai\n");
     printf("hello world from shai branch named: shaiTest2\n");
     printf("new test time: 21:11\n");
+    printf("new commit from master\n");
     return 0;
 }
