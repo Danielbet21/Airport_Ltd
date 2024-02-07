608515a4c4fbce2f7fa48bda8fb33f1dba97bc6b
shai <yayaya142@gmail.com>
2024-02-07 21:25:57 +0200

    clean all tests

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("hell world from shai\n");
-    printf("hello world from shai branch named: shaiTest2\n");
-    printf("new test time: 21:11\n");
-    printf("new commit from master\n");
     return 0;
 }
