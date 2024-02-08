3c4992698e0dcef1182f85b571b3fcc67be800bc
shai <yayaya142@gmail.com>
2024-02-08 17:48:22 +0200

    remove prgma once and add 'ifndef'

--- generalStrings.h
+++ generalStrings.h
@@ -1,6 +1,12 @@
-#pragma once
+#ifndef __GENERAL_STRINGS__
+#define __GENERAL_STRINGS__
+
+
+
 
 #define MAX_LENGTH 255
 
 char*	getStrExactLength(const char* msg);
 char* 	myGets(char* buf, int size);
+
+#endif
