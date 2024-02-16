57d6c1bb19fdfd976f702ec58952f1887fec8e35
shai <yayaya142@gmail.com>
2024-02-16 17:53:32 +0200

    fix bugs with flight change overall functions

--- flight.c
+++ flight.c
@@ -1,38 +1,75 @@
-//#include "stdio.h"
-//#include "flight.h"
-//
-//
-//Flight initFlight(){
-//    Flight flight;
-//    flight.plane_data;
-//    flight.airport_c_Tar = ;
-//    flight.airport_c_sour = ;
-//    flight.date = date_function();
-//
-//    return flight;
-//}
-//
-//
-//
-//int isFlightFromSourceAirport(Flight *flight, int airport_code_src){
-//    if(flight->airport_c_sour == airport_code_src){
-//        return 1;
-//    }else{
-//        return 0;
-//    }
-//}
-//int isFlightToDestAirport(Flight *flight, int airport_code_des){
-//    if(flight->airport_c_Tar == airport_code_des){
-//        return 1;
-//    }else{
-//        return 0;
-//    }
-//}
-//
-//int isPlanTypeInFlight(Flight *flight,Plane *plane){
-//    if(flight->plane_data->****** == plane->******){
-//        return 1;
-//    }else{
-//        return 0;
-//    }
-//}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "flight.h"
+
+char *chooseAirport(Flight *pFlight, const AirportManager *pManager, int isSource){
+    // set the output message
+    char *outPutMessage = "origin";
+    int validCode = 0;
+    char *airportCode;
+    if (!isSource) {
+        outPutMessage = "destination";
+    }
+    // print all the airport from the airport manager
+    printAllAirports(pManager);
+//  get airport code from user until successfully
+    do {
+        printf("Enter code of %s airport:",outPutMessage);
+        airportCode = getStrExactLength(NULL);
+        validCode = codeExist(pManager, airportCode);
+        if (validCode &&!isSource && strcmp(pFlight->airport_c_sour,airportCode) == 0 ) {
+//          check the destination code is not source code
+            printf("Same origin and destination airport\n");
+          validCode = 0;
+        }
+    } while (!validCode);
+    return airportCode;
+}
+
+int initFlight(Flight *pFlight,const AirportManager *pManager,Plane *pPlane){
+    // if plane is NULL will free the flight and return 0
+    if(pManager->airportLength < 2 || !pPlane) {
+        freeFlight(pFlight);
+        return 0;
+    }
+    // set the airport codes (for source and destination)
+    pFlight->airport_c_sour = chooseAirport(pFlight,pManager, 1);
+    pFlight->airport_c_Tar = chooseAirport(pFlight, pManager, 0);
+    // set date and insert plane
+    Date d;
+    getCorrectDate(&d);
+    pFlight->date = d;
+    pFlight->plane = pPlane;
+    return 1;
+}
+
+int isFlightFromSourceAirport(const Flight *flight, const char* airport_code_src){
+    if (strcmp(flight->airport_c_sour, airport_code_src) == 0) {
+        return 1;
+    }
+    return 0;
+}
+int isFlightToDestAirport(const Flight *flight, const char* airport_code_des){
+    if (strcmp(flight->airport_c_Tar, airport_code_des) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int isPlaneTypeInFlight(const Flight *flight, PlaneType planeType){
+    return (flight->plane->type == planeType);
+}
+
+void freeFlight(Flight *pFlight) {
+        free(pFlight->airport_c_sour);
+        free(pFlight->airport_c_Tar);
+}
+void printFlight(const Flight *pFlight){
+    printf("Flight From %s to %s  ",pFlight->airport_c_sour, pFlight->airport_c_Tar);
+    printDate(&pFlight->date);
+    printf(" ");
+    printPlane(pFlight->plane);
+    printf("\n");
+}
+
--- flight.h
+++ flight.h
@@ -1,18 +1,26 @@
-//#ifndef FLIGHT_H
-//#define FLIGHT_H
-//
-//typedef struct{
-//    int airport_c_sour;
-//    int airport_c_Tar;
-//    Plane *plane_data;
-//    char *date;
-//}Flight;
-//
-//Flight initFlight();
-//
-//int isFlightFromSourceAirport(Flight *flight, int airport_code);
-//
-//int isFlightToDestAirport(Flight *flight, int airport_code_des);
-//
-//int isPlanTypeInFlight(Flight *flight,Plane *plane);
-//#endif //FLIGHT_H
+#ifndef __FLIGHT_H__
+#define __FLIGHT_H__
+
+#include "plane.h"
+#include "date.h"
+#include "airportManager.h"
+#include "generalStrings.h"
+
+typedef struct {
+    char *airport_c_sour;
+    char *airport_c_Tar;
+    Plane *plane;
+    Date date;
+} Flight;
+
+
+int initFlight(Flight *pFlight, const AirportManager *pManager,Plane *pPlane);
+int isFlightFromSourceAirport(const Flight *flight, const char* airport_code);
+int isFlightToDestAirport(const Flight *flight, const char* airport_code_des);
+int isPlaneTypeInFlight(const Flight *flight, PlaneType planeType);
+void freeFlight(Flight *pFlight);
+void printFlight(const Flight *pFlight);
+char *chooseAirport(Flight *pFlight, const AirportManager *pManager,  int isSource);
+
+
+#endif
