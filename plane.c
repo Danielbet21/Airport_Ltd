1eccd3ac18d14e4ea8cb0090d1a2b80ca7cb8b58
Daniel <danielbetzalel16@gmail.com>
2024-02-17 11:21:36 +0200

    add fixed plane+date

--- plane.c
+++ plane.c
@@ -1,31 +1,52 @@
-//#include <stdio.h>
-//#include "plane.h"
-//
-//#define MIN_SERIAL_NUM 1
-//#define MAX_SERIAL_NUM 9999
-//
-//
-//int initPlane(Plane *pPlane, int serialNum, PlaneType type) {
-//    if (serialNum < MIN_SERIAL_NUM || serialNum > MAX_SERIAL_NUM) {
-//        return 0;
-//    }
-//    pPlane->serialNum = serialNum;
-//    pPlane->type = type;
-//    return 1;
-//};
-//
-//PlaneType getTypeFromUser(){
-//    int userSelect = -1;
-//    do {
-//        printf("Please enter one of the following types\n");
-//        for (int i = 0; i < NofPlaneTypes; i++) {
-//            printf("%d for %s\n", i, PlaneTypesTitle[i]);
-//        }
-//        scanf("%d", &userSelect);
-//    } while (userSelect < 0 || userSelect >= NofPlaneTypes);
-//};
-//
-//void printPlane(const Plane *pPlane){
-//    printf("NEED TO CHANGE PLANE PRINT! IN plane.c file\n");
-//    printf("Plane Type: %s, serial number: %d\n", PlaneTypesTitle[pPlane->type], pPlane->serialNum);
-//};
+#include <stdio.h>
+#include "plane.h"
+
+#define MIN_SERIAL_NUM 1
+#define MAX_SERIAL_NUM 9999
+
+
+void initPlane(Plane* pPlane, Plane* planeArr, int planeCount) {
+    int serialNumber;
+//  get the serial number from the user and check if unique and valid
+    do {
+        serialNumber = getSerialNumberFromUser();
+    } while (!checkSerialNumber(serialNumber,planeArr,planeCount));
+    pPlane->serialNum = serialNumber;
+    pPlane->type = getTypeFromUser();
+}
+
+int checkSerialNumber(int serialNumber, Plane* planeArr, int planeCount){
+//    check if serial number is valid
+    if (serialNumber < MIN_SERIAL_NUM || serialNumber > MAX_SERIAL_NUM) {
+        return 0;
+    }
+//  check if the serial number is unique
+    for (int i = 0; i < planeCount; i++) {
+        if (planeArr[i].serialNum == serialNumber) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int getSerialNumberFromUser() {
+    printf("Enter plane serial number - between %d to %d\n",MIN_SERIAL_NUM, MAX_SERIAL_NUM);
+    int serialNumber;
+    scanf("%d",&serialNumber);
+    return serialNumber;
+}
+
+PlaneType getTypeFromUser(){
+    int userSelect = -1;
+    do {
+        printf("Please enter one of the following types\n");
+        for (int i = 0; i < NofPlaneTypes; i++) {
+            printf("%d for %s\n", i, PlaneTypesTitle[i]);
+        }
+        scanf("%d", &userSelect);
+    } while (userSelect < 0 || userSelect >= NofPlaneTypes);
+};
+
+void printPlane(const Plane *pPlane){
+    printf("Plane: serial number:%d, type %s\n", pPlane->serialNum, PlaneTypesTitle[pPlane->type]);
+};
--- plane.h
+++ plane.h
@@ -1,19 +1,21 @@
-//#ifndef __PLANE__
-//#define __PLANE__
-//
-//typedef enum {Commercial, Cargo, Military, NofPlaneTypes} PlaneType;
-//static const char *PlaneTypesTitle[NofPlaneTypes] = {"Commercial", "Cargo","Military"};
-//
-//// plane struct
-//typedef struct {
-//    PlaneType type;
-//    int serialNum;
-//} Plane;
-//
-//
-//// functions
-//int initPlane(Plane *pPlane, int serialNum, PlaneType type);
-//PlaneType getTypeFromUser();
-//void printPlane(const Plane *pPlane);
-//
-//#endif
+#ifndef __PLANE__
+#define __PLANE__
+
+typedef enum {Commercial, Cargo, Military, NofPlaneTypes} PlaneType;
+static const char *PlaneTypesTitle[NofPlaneTypes] = {"Commercial", "Cargo","Military"};
+
+// plane struct
+typedef struct {
+    PlaneType type;
+    int serialNum;
+} Plane;
+
+
+// functions
+void initPlane(Plane* pPlane, Plane* planeArr, int planeCount);
+PlaneType getTypeFromUser();
+void printPlane(const Plane *pPlane);
+int getSerialNumberFromUser();
+int checkSerialNumber(int SerialNumber, Plane* planeArr, int planeCount);
+
+#endif
